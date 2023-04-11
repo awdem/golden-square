a8a88c4d93502a2d4fbb4ff6dbb45e60fd3cbfbc
William de Montmollin <awdem@MASTAFF-Joe-Charm-C17GMB2AQ6L4.local>
2023-04-11 17:11:04 +0100

    added 2nd test to previous exercise

--- phase_one/spec/counter_spec.rb
+++ phase_one/spec/counter_spec.rb
@@ -7,4 +7,12 @@ RSpec.describe Counter do
         result = counter.report
         expect(result).to eq "Counted to 4 so far."
     end
+        it "adds an integer to a counter and reports the current sum" do
+        counter = Counter.new
+        counter.add(4)
+        counter.add(6)
+        counter.add(0)
+        result = counter.report
+        expect(result).to eq "Counted to 10 so far."
+    end
 end
