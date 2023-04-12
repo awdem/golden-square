838d95ea0c1338e08604b7ee9d41cc9d341d5604
William de Montmollin <awdem@MASTAFF-Joe-Charm-C17GMB2AQ6L4.local>
2023-04-12 10:14:26 +0100

    finished class tests exercise 2

--- phase_one/spec/string_builder_spec.rb
+++ phase_one/spec/string_builder_spec.rb
@@ -1,14 +1,14 @@
 require "string_builder"
 
 RSpec.describe StringBuilder do
-    it "adds input to a string and returns the string" do
+    it "add string inputs to a local variable and returns the string" do
         string = StringBuilder.new
         string.add("big")
         string.add("bigger")
         result = string.output   
         expect(result).to eq "bigbigger"
     end
-    it "adds input to a string and returns the string length" do
+    it "add string inputs to a local variable and returns the string length" do
         string = StringBuilder.new
         string.add("big")
         string.add("bigger")
