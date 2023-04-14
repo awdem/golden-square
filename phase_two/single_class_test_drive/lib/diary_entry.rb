1dba61ab42e4fbe798b431f7d27a37c3e98bd258
awdem <wdemontmollin@gmail.com>
2023-04-14 12:16:18 +0100

    added test drive single class exercise in progress

--- phase_two/single_class_test_drive/lib/diary_entry.rb
+++ phase_two/single_class_test_drive/lib/diary_entry.rb
@@ -1,7 +1,10 @@
 class DiaryEntry
   def initialize(title, contents) # title, contents are strings
+    fail "No text in title!" if title.empty?
+    fail "No text in contents!" if contents.empty?
     @title = title
     @contents = contents
+    @chunk = []
   end
 
   # Returns the title as a string
@@ -18,21 +21,43 @@ class DiaryEntry
   def count_words
     contents.split(" ").size
   end
-# continue from here
-  def reading_time(wpm) # wpm is an integer representing the number of words the
-                        # user can read per minute
-    # Returns an integer representing an estimate of the reading time in minutes
-    # for the contents at the given wpm.
+# wpm is an integer representing the number of words the
+# user can read per minute
+# Returns an integer representing an estimate of the reading time in minutes
+# for the contents at the given wpm.
+  def reading_time(wpm) 
+    fail "wpm must be above 0" if wpm  <= 0
+    return (count_words / wpm.to_f).round
   end
 
-  def reading_chunk(wpm, minutes) # `wpm` is an integer representing the number
-                                  # of words the user can read per minute
-                                  # `minutes` is an integer representing the
-                                  # number of minutes the user has to read
-    # Returns a string with a chunk of the contents that the user could read
-    # in the given number of minutes.
-    # If called again, `reading_chunk` should return the next chunk, skipping
-    # what has already been read, until the contents is fully read.
-    # The next call after that it should restart from the beginning.
+# `wpm` is an integer representing the number
+# of words the user can read per minute
+# `minutes` is an integer representing the
+# number of minutes the user has to read
+# Returns a string with a chunk of the contents that the user could read
+# in the given number of minutes.
+# If called again, `reading_chunk` should return the next chunk, skipping
+# what has already been read, until the contents is fully read.
+# The next call after that it should restart from the beginning.
+
+  def reading_chunk(wpm, minutes)
+    readable_words = wpm * minutes 
+    @chunk = [] if @chunk.size < readable_words
+    if @chunk.empty?
+      @chunk = words[0, readable_words]
+      return @chunk.join(" ")
+    else 
+      words[@chunk.length, readable_words]
+      @chunk = words[@chunk.length, readable_words]
+      return @chunk.join(" ")
+    end
   end
+
+
+  private
+
+  def words
+    @contents.split(" ")
+  end
+  
 end
--- phase_two/single_class_test_drive/spec/diary_entry_spec.rb
+++ phase_two/single_class_test_drive/spec/diary_entry_spec.rb
@@ -10,21 +10,101 @@ require "diary_entry"
 # end
 
 RSpec.describe DiaryEntry do
-  context "given a title and contents" do
-    it "returns the title as a string" do
-      entry = DiaryEntry.new("day one", "this is the end of day one.")
-      expect(entry.title).to eq "day one"
+  describe "#initialize" do
+    context "given a title and contents" do
+      it "constructs" do
+        entry = DiaryEntry.new("day one", "this is the end of day one.")
+        expect(entry.title).to eq "day one"
+        expect(entry.contents).to eq "this is the end of day one."
+      end  
     end
 
-    it "returns the contents as a string" do
-      entry = DiaryEntry.new("day one", "this is the end of day one.")
-      expect(entry.contents).to eq "this is the end of day one."
-    end
+    context "given an empty string as title" do
+      it "fails" do
+        expect { entry = DiaryEntry.new("", "this is the end of day one.") }.to raise_error "No text in title!"
+      end  
+    end 
+    
+    context "given an empty string as contents" do
+      it "fails" do
+        expect { entry = DiaryEntry.new("day one", "") }.to raise_error "No text in contents!"
+      end  
+    end      
+  end
 
-    it "Returns the number of words in the contents as an integer" do
+  describe "#title" do
+    context "given contents" do
+      it "returns the title as a string" do
+        entry = DiaryEntry.new("day one", "this is the end of day one.")
+        expect(entry.title).to eq "day one"
+      end
+    end
+  end
+
+  describe "#contents" do
+    context "given contents" do
+      it "returns the contents as a string" do
+        entry = DiaryEntry.new("day one", "this is the end of day one.")
+        expect(entry.contents).to eq "this is the end of day one."
+      end
+    end
+  end
+
+  describe "#count_words" do
+    it "returns the number of words in contents as an integer" do
       entry = DiaryEntry.new("day one", "this is the end of day one.")
       expect(entry.count_words).to eq 7
     end
-
   end
+
+  describe "#reading_time"
+    context "given an arbitrary wpm (250)" do
+      it "returns the ceiling of minutes to read contents as an integer" do
+        entry = DiaryEntry.new("day one", ("one " * 575))
+        expect(entry.reading_time(250)).to eq 2
+      end
+    end
+
+    context "given an arbitrary wpm of 0" do
+      it "fails" do
+        entry = DiaryEntry.new("day one", ("one " * 575))
+        expect{ entry.reading_time(0) }.to raise_error "wpm must be above 0"
+      end
+    end
+  
+  describe "#reading_chunk" do
+    context "given a text readable in the given minutes" do
+      it "returns the whole of contents" do
+        entry = DiaryEntry.new("day one", "one two three")
+        chunk = entry.reading_chunk(3, 1)
+        expect(chunk).to eq ("one two three")
+      end
+    end
+    
+    context "given a text that must be read in two chunks" do
+      it "returns the first half when called once" do
+        entry = DiaryEntry.new("day one", ("one two three four five"))
+        chunk = entry.reading_chunk(4, 1)
+        expect(chunk).to eq ("one two three four")
+      end 
+
+      it "returns the first half and the second half when called twice" do
+        entry = DiaryEntry.new("day one", ("one two three four five"))
+        entry.reading_chunk(4, 1)
+        chunk = entry.reading_chunk(4, 1)
+        expect(chunk).to eq ("five")
+      end
+      
+      it "resets the chunk once the full contents have been read" do
+        entry = DiaryEntry.new("day one", ("one two three four five"))
+        entry.reading_chunk(4, 1)
+        entry.reading_chunk(4, 1)
+        chunk = entry.reading_chunk(4, 1)
+        expect(chunk).to eq ("one two three four")
+      end
+      # what if it ends exactly on the word count.
+      # does it fail if wpm is 0
+    end
+  end
+
 end
