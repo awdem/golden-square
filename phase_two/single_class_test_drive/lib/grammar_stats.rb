6c3a52e4afa9fb42a0f31afe4fe788f301e3a6cd
Louis Claremont <louisclaremont@MASTUDENT-Louis-Claremont-C17GMB3FQ6L4.local>
2023-04-14 14:58:39 +0100

    test drive class challenge

--- phase_two/single_class_test_drive/lib/grammar_stats.rb
+++ phase_two/single_class_test_drive/lib/grammar_stats.rb
@@ -1,15 +1,25 @@
 class GrammarStats
   def initialize
-    # ...
+    @text_counter = 0
+    @passed_tests = 0
   end
 
   def check(text) 
     fail "empty string not valid" if text == ""
-    text[0] == text[0].upcase && "!.?".include?(text[-1])
+    if text[0] == text[0].upcase && "!.?".include?(text[-1])
+      @passed_tests += 1
+      @text_counter += 1
+      return true
+    else
+      @text_counter += 1
+      return false
+    end
   end
 
-  def percentage_good
-    # Returns as an integer the percentage of texts checked so far that passed
+  # Returns as an integer the percentage of texts checked so far that passed
     # the check defined in the `check` method. The number 55 represents 55%.
+  def percentage_good
+    return 0 if @text_counter == 0
+    ((@passed_tests / @text_counter.to_f) * 100).round
   end
 end
--- phase_two/single_class_test_drive/spec/grammar_stats_spec.rb
+++ phase_two/single_class_test_drive/spec/grammar_stats_spec.rb
@@ -36,7 +36,56 @@ RSpec.describe GrammarStats do
         expect(is_punctual).to eq false
       end
     end
+  end
 
+  describe '#percentage_good' do
+    context 'if nothing has been checked' do
+      it 'returns 0' do
+        grammar_stats = GrammarStats.new
+        results = grammar_stats.percentage_good
+        expect(results).to eq 0
+      end
+    end
 
+    context 'if half checks pass' do
+      it 'returns 50' do
+        grammar_stats = GrammarStats.new
+        text1 = 'Hello!'
+        text2 = 'Hello again'
+        grammar_stats.check(text1)
+        grammar_stats.check(text2)
+
+        results = grammar_stats.percentage_good
+        expect(results).to eq 50
+      end
+    end
+
+    context 'if two thirds of checks pass' do
+      it 'returns 67' do
+        grammar_stats = GrammarStats.new
+        text1 = 'Hello!'
+        text2 = 'Hello again'
+        text3 = 'Hello x3.'
+        grammar_stats.check(text1)
+        grammar_stats.check(text2)
+        grammar_stats.check(text3)
+        results = grammar_stats.percentage_good
+        expect(results).to eq 67
+      end
+    end
+
+    context 'if percentage needs to be rounded down' do
+      it 'returns 33' do
+        grammar_stats = GrammarStats.new
+        text1 = 'Hello,'
+        text2 = 'Hello again'
+        text3 = 'Hello x3.'
+        grammar_stats.check(text1)
+        grammar_stats.check(text2)
+        grammar_stats.check(text3)
+        results = grammar_stats.percentage_good
+        expect(results).to eq 33
+      end
+    end
   end
 end
