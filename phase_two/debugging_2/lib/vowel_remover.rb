f9e30862e9fd4ab2e8a58054fbce4f8e118c647b
awdem <wdemontmollin@gmail.com>
2023-04-17 11:27:03 +0100

    finished exercise debugging 2

--- phase_two/debugging_2/lib/vowel_remover.rb
+++ phase_two/debugging_2/lib/vowel_remover.rb
@@ -1,3 +1,24 @@
+###### original code:
+# class VowelRemover
+#   def initialize(text)
+#     @text = text
+#     @vowels = ["a", "e", "i", "o", "u"]
+#   end
+
+#   def remove_vowels()
+#     i = 0
+#     while i < @text.length()
+#       binding.irb
+#       if @vowels.include? @text[i].downcase
+#         @text = @text.slice(0,i) + @text.slice(i+1..-1)
+#       end
+#       i += 1
+#     end
+#     return @text
+#   end
+# end
+
+#  debugged code:
 class VowelRemover
   def initialize(text)
     @text = text
@@ -7,12 +28,36 @@ class VowelRemover
   def remove_vowels()
     i = 0
     while i < @text.length()
-      binding.irb
       if @vowels.include? @text[i].downcase
         @text = @text.slice(0,i) + @text.slice(i+1..-1)
       end
-      i += 1
+      i += 1 unless (@text.empty? || @vowels.include?(@text[i].downcase))
     end
     return @text
   end
 end
+
+remover = VowelRemover.new("Aardvarks are cool.")
+p remover.remove_vowels
+
+
+###### video debugged code:
+# class VowelRemover
+#   def initialize(text)
+#     @text = text
+#     @vowels = ["a", "e", "i", "o", "u"]
+#   end
+
+#   def remove_vowels()
+#     i = 0
+#     while i < @text.length()
+#       binding.irb
+#       if @vowels.include? @text[i].downcase
+#         @text = @text.slice(0,i) + @text.slice(i+1..-1)
+#       else
+#       i += 1
+#       end
+#     end
+#     return @text
+#   end
+# end
--- phase_two/debugging_2/spec/vowel_remover_spec.rb
+++ phase_two/debugging_2/spec/vowel_remover_spec.rb
@@ -12,4 +12,29 @@ RSpec.describe "remove_vowels method" do
     result_no_vowels = remover.remove_vowels
     expect(result_no_vowels).to eq "W wll rmv th vwls frm ths sntnc."
   end
+  
+  context "given 'aeiou'" do          
+    it "returns an empty string" do
+      remover = VowelRemover.new("aeiou")
+      result_no_vowels = remover.remove_vowels
+      expect(result_no_vowels).to eq ""
+    end
+  end  
+
+  context "given a string that starts with a vowel" do          
+    it "removes the vowels" do
+      remover = VowelRemover.new("All that I am.")
+      result_no_vowels = remover.remove_vowels
+      expect(result_no_vowels).to eq "ll tht  m."
+    end
+  end
+
+  context "given a string that starts with two vowels" do          
+    it "removes the vowels" do
+      remover = VowelRemover.new("Aardvarks are cool.")
+      result_no_vowels = remover.remove_vowels
+      expect(result_no_vowels).to eq "rdvrks r cl."
+    end
+  end
+   
 end
